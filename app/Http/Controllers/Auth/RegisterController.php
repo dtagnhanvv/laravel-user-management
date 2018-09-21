9269f288e9d573d4a720e285d5a0f9db443ee5fe
jennyphuong <dtag.nhanvv@gmail.com>
2018-09-21 21:09:41 +0700

    User: support address and tel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'address' => $data['address'],
+            'tel' => $data['tel'],
             'password' => Hash::make($data['password']),
         ]);
     }
